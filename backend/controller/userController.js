4b35735e3536c1f9ce197ac5de7be90c668c2a38
netra405 <netrarajbanshi18@gmail.com>
2025-10-15 10:23:36 +0545

    model, controller, and route for create channel is completed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,6 @@
+import Channel from "../model/channelModel.js"
 import User from "../model/userModel.js"
+import uploadOnCloudinary from "../config/cloudinary.js"
 
 
 
@@ -15,3 +17,47 @@ export const getCurrentUser = async (req,res)=>{
         res.status(500).json({message:`getCurrentUser error ${error}`})
     }
 }
+
+
+
+export const createChannel = async (req, res) => {
+     try {
+        const {name, description, category} = req.body;
+        const userId = req.userId;
+        const existingChannel = await Channel.findOne({owner:userId})
+        if (existingChannel) {
+            return res.status(400).json({message:"User already  have a channel."})
+        }
+        const nameExists = await Channel.findOne({name})
+        if (nameExists) {
+            return res.status(400).json({message:"Channel name already taken"})
+        }
+        let avatar
+        let banner
+        if (req.files?.avatar) {
+            avatar = await uploadOnCloudinary(req.files.avatar[0].path)
+        }
+        if (req.files?.banner) {
+            banner = await uploadOnCloudinary(req.files.banner[0].path)
+        }
+        const channel = await Channel.create({
+            name,
+            description,
+            category,
+            avatar,
+            banner,
+            owner:userId
+        })
+        await User.findByIdAndUpdate(userId , {
+            channel: channel._id,
+            userName:name,
+            photoUrl:avatar
+        })
+
+        return res.status(201).json(channel)
+     } catch (error) {
+        return res.status(500).json({message:`Create channel error ${error}`})
+     }
+
+}
+
--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,9 +1,15 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import { getCurrentUser } from "../controller/userController.js"
+import { createChannel, getCurrentUser } from "../controller/userController.js"
+import upload from "../middleware/multer.js"
 
 const userRouter = express.Router()
 
 userRouter.get("/getuser",isAuth,getCurrentUser)
+userRouter.post("/createChannel", isAuth, upload.fields([
+   { name: "avatar", maxCount: 1},
+    {name: "banner", maxCount: 1}
+
+]) , createChannel)
 
 export default userRouter
